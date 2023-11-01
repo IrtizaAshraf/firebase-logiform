6c44a6a5719b9f8f37c878b828df858d3d00d47f
Irtiza Ashraf <irtizaashraf802@gmail.com>
2023-11-02 01:25:22 +0500

    solve register issue

--- login.js
+++ login.js
@@ -1,51 +1,51 @@
 /// login form java script-------- start //
 
 // const   loginForm =document.querySelector('#loginForm')
-loginForm.addEventListener('submit', function(event) {
-    event.preventDefault();
+// loginForm.addEventListener('submit', function(event) {
+//     event.preventDefault();
 
-//     const enteredUsername = document.getElementById('username').value;
-//     const enteredPassword = document.getElementById('password').value;
+// //     const enteredUsername = document.getElementById('username').value;
+// //     const enteredPassword = document.getElementById('password').value;
 
-    const userData = JSON.parse(localStorage.getItem('user')) || [];
+//     const userData = JSON.parse(localStorage.getItem('user')) || [];
     
-    let matchedUserdata = false;
+//     let matchedUserdata = false;
     
-    // Loop through user data to check for a match
-    for (const user of userData) {
+//     // Loop through user data to check for a match
+//     for (const user of userData) {
     
-        if (user.username === enteredUsername && user.password === enteredPassword) {
-            matchedUserdata = true;
+//         if (user.username === enteredUsername && user.password === enteredPassword) {
+//             matchedUserdata = true;
             
         
-            break; // match the value and exite the loop
-        }
+//             break; // match the value and exite the loop
+//         }
      
-    }
+//     }
     
-    if (matchedUserdata) {
+//     if (matchedUserdata) {
        
-        Swal.fire({
-            title: 'Login Successful!',
-            text: 'Redirecting to the homepage...',
-            icon: 'success',
-            showConfirmButton: false,
-            timer: 1500,
-            willClose: () => {
-                window.location.href = 'index.html'; // Redirect after the alert is closed
-            }
-        });
+//         Swal.fire({
+//             title: 'Login Successful!',
+//             text: 'Redirecting to the homepage...',
+//             icon: 'success',
+//             showConfirmButton: false,
+//             timer: 1500,
+//             willClose: () => {
+//                 window.location.href = 'index.html'; // Redirect after the alert is closed
+//             }
+//         });
         
 
         
-    } else {
-        Swal.fire({
-            title: 'Login Failed!',
-            text: 'Please check your username and password.',
-            icon: 'error'
-        });
-    }
-});
+//     } else {
+//         Swal.fire({
+//             title: 'Login Failed!',
+//             text: 'Please check your username and password.',
+//             icon: 'error'
+//         });
+//     }
+// });
 
 /// login form java script-------- end //
 
---
+++ register.js
@@ -0,0 +1,58 @@
+
+// import { createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js';
+// import { auth } from './config.js';
+
+
+// const registrationForm = document.getElementById('registrationForm');
+// const username = document.getElementById('username').value;
+// const email = document.getElementById('email').value;
+// const password = document.getElementById('password').value;
+
+// registrationForm.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     createUserWithEmailAndPassword(auth, email.value, password.value)
+//   .then((userCredential) => {
+   
+      //     const user = userCredential.user;
+      //     console.log(user);
+      //   })
+      //   .catch((error) => {
+            //     const errorCode = error.code;
+            //     const errorMessage = error.message;
+            // //    console.log(errorCode);
+            //   });
+            // })
+            
+            
+            
+            
+            
+            
+            
+            
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+import { auth } from "./config.js";
+            
+            
+const registrationForm = document.querySelector('#registrationForm');
+const form = document.querySelector('#form');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+
+
+registrationForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    createUserWithEmailAndPassword(auth, email.value, password.value)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            console.log(user);
+            email.value = ''
+            password.value = ''
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorMessage);
+        });
+
+})
