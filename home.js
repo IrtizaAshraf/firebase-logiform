85eaccdffa086650912bca0d1e2e83f555330f9e
Irtiza Ashraf <irtizaashraf802@gmail.com>
2023-11-08 01:45:31 +0500

    reload problem solve

--- home.js
+++ home.js
@@ -4,7 +4,7 @@
 import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
 import { auth, db } from "./config.js";
 // firestore link pastt -------->
-import { collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
+import { collection, addDoc, getDocs, query, Timestamp, orderBy } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
 
 
 
@@ -37,45 +37,54 @@ logout.addEventListener('click', () => {
 
 
 
-const card = document.querySelectorAll('#posts')
 
 // Add your JavaScript code here for interactivity
+const title=document.querySelector('#tittle')
 
 document.getElementById("post-form").addEventListener("submit", async (event) => {
       event.preventDefault();
       var postText = document.getElementById("post-text").value;
       // console.log(auth.currentUser.uid);
       try {
+            
             const docRef = await addDoc(collection(db, "post"), {
-                  Tittle: postText,
-                  //   discription:discription.value,
-                  uid: auth.currentUser.uid
+                  Tittle: title.value,
+                  discription:postText,
+                  uid: auth.currentUser.uid,
+                  postDate: Timestamp.fromDate(new Date()),
+
             });
             console.log("Document written with ID: ", docRef.id);
+            gateDatafirebase()
+
       } catch (e) {
             console.error("Error adding document: ", e);
       }
       // Check if postText is not empty
-      if (postText.trim() !== "") {
-            var postsContainer = document.getElementById("posts");
-            var postElement = document.createElement("div");
-            postElement.className = "post";
-            postElement.innerHTML = "<h2>User</h2><p>" + postText + "</p>";
-            postsContainer.appendChild(postElement);
-            document.getElementById("post-text").value = "";
-      } else {
-            // Handle the case when postText is empty (or contains only whitespace)
-            alert("Please enter a valid post.");
-      }
+      // if (postText.trim() !== "") {
+      //       var postsContainer = document.getElementById("posts");
+      //       var postElement = document.createElement("div");
+      //       postElement.className = "post";
+      //       postElement.innerHTML = "<h2>User</h2><p>" + postText + "</p>";
+      //       postsContainer.appendChild(postElement);
+      //       document.getElementById("post-text").value = "";
+      // } else {
+      //       // Handle the case when postText is empty (or contains only whitespace)
+      //       alert("Please enter a valid post.");
+      // }
 });
 
 
 // get firebasedata  in firestore------------>
 
+const card = document.querySelector('#card');
 
 let arry = []
 async function gateDatafirebase() {
-      const querySnapshot = await getDocs(collection(db, "post"));
+      arry.length= 0;
+      card.innerHTML= '';
+      const postsQuery = query(collection(db, "post"), orderBy('postDate', 'desc'));
+      const querySnapshot = await getDocs(postsQuery);
       querySnapshot.forEach((doc) => {
             arry.push(doc.data());
       });
@@ -84,13 +93,14 @@ async function gateDatafirebase() {
             card.innerHTML += `
             <div class="card" style="width: 18rem;">
             <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <h5 class="card-title">${item.Tittle}</h5>
+            <p class="card-text">${item.discription}</p>
+            <a href="#" class="btn btn-primary"></a>
             </div>
             </div>
-            <img src="..." class="card-img-top" alt="...">
+          
             `
+          
       })
 }
 
